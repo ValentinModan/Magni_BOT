fe5650b4492215a7044a8233c3b02a17de73befd
Valentin Modan <valentinmodan@yahoo.com>
2021-07-30 20:51:36 +0300

    Second commit
    testing requirement id when pushing

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Testing the responses");
+
+    //    PostRequests.sendRequest();
+//
+    }
+}
---
+++ src/main/java/PostRequests.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class PostRequests {
+
+    public static URL url;
+
+    private static void setUp() {
+        try {
+            URL url = new URL("https://lichess.org/");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendRequest()
+    {
+        try {
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+
+            //set request time to POSt
+            con.setRequestMethod("POST");
+
+            //set properties
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+
+            con.setDoOutput(true);
+
+           String jsonInputString = "{'name': 'Upendra', 'job': 'Programmer'}";
+
+            try(OutputStream os = con.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            try(BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+            }
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
